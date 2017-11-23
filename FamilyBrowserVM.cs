065a1cf10fbc0b9441220945dfdf1f1dcb93dfd2
XGH <ghxiong@incarcloud.com>
2017-11-23 23:42:33 +0800

    面板布局

---
+++ FamilyBrowserVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitHP
+{
+    class FamilyBrowserVM
+    {
+        public FamilyBrowserVM()
+        {
+            this.status = "I'm Ready!";
+        }
+
+        public string status { get; set; }
+    }
+}
