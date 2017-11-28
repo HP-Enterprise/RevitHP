acc9a02d15df0e1b36f25c6b9487a360074b6397
XGH <ghxiong@incarcloud.com>
2017-11-28 16:38:07 +0800

    API description

--- RevitBiz.cs
+++ RevitBiz.cs
@@ -74,6 +74,22 @@ namespace RevitHP
         private void pull()
         {
             // TODO: 从远程拉一个数据库的新版本下来
+
+            /* REQUEST 请求
+             * HTTP GET /{prefix}/catalog
+             * HTTP HEAD
+             *     If-None-Match: 可选.内容的MD5值(32个字符的大写16进制字符串形式，如 52291B5CECA35EEFABB4532CC462E1EC)
+             * 
+             * RESPONSE 响应
+             * HTTP HEAD
+             *     ETag: 内容的MD5值(32个字符的大写16进制字符串形式)
+             *     Content-Type: application/octet-stream 字节流
+             * HTTP Status Code
+             *     200: 请求的If-None-Match的值不匹配(或没有发送If-None-Match)时,返回内容的二进制字节流
+             *     304: 请求的If-None-Match值匹配时,不需要返回内容
+             *     404: 如果客户端从来没有上传过数据，返回404错误
+             * HTTP Content 可选.内容的二进制字节流
+             */
         }
 
         private void push()
@@ -82,6 +98,19 @@ namespace RevitHP
             // 为了安全，强制要求远程服务器执行检查逻辑
             // PUSH时带上修订前数据库的MD5值，如果和服务器端的不匹配，表明已经有其它修订发生
             // 服务器端直接返回失败,要求客户端重新PULL新的修订版后再重试PUSH
+
+            /* REQUEST 请求
+             * HTTP POST /{prefix}/catalog
+             * HTTP HEAD
+             *     If-Match: 期望服务器上的旧数据匹配此值(MD5的大写16进制字符串形式32个字符)
+             *     Content-Type: application/octet-stream 字节流
+             * HTTP Content 内容的二进制字节流
+             * 
+             * RESPONSE 响应
+             * HTTP Status Code
+             *     204: 请求中If-Match的内容和服务器上旧数据的值匹配，服务器用请求里的内容更新服务器上的数据
+             *     412: 请求中If-Match的内容和服务器上旧数据的值不匹配，服务器拒绝请求，不更新数据，且服务器也不需要读取请求的内容
+             */
         }
 
         public ObservableCollection<CataItem> LoadCatalog()
